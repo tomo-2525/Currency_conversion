45a60d17bd4e21e62274da70c977f8256a668eb8
tomo-2525 <tomoblog.0205@gmail.com>
2020-02-21 02:18:53 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+var selectedCurrency = "ドル"; //  グローバル変数
+
+function changeCurrency() {
+    var currency = document.getElementById("currency");
+
+    selectedCurrency = currency.options[currency.selectedIndex].value;
+
+    var rateElement = document.getElementById("rate");
+
+    switch (selectedCurrency) {
+        case "ドル":
+            rateElement.value = 110;
+            break;
+
+        case "ペリカ(カイジ)":
+            rateElement.value = 0.1;
+            break;
+
+        case "ゴールド(ドラクエ)":
+            rateElement.value = 50;
+            break;
+
+            dafault:
+            rareElement.value = -1;
+            break;
+    }
+
+    document.getElementById("buttonExchange").value = selectedCurrency + "から円に両替";
+    document.getElementById("buttonExchange2").value = "円から" + selectedCurrency + "に両替";
+
+}
+
+
+
 function exchange(excahngeType) {
     var rate = document.getElementById("rate").value;
     var inMoney = document.getElementById("inMoney").value;
@@ -8,10 +42,26 @@ function exchange(excahngeType) {
     if (excahngeType == "toYen") {
         exchangeMoney = inMoney * rate;
         currency = "円";
+        var currencyfrom = document.getElementById("currencyFrom");
+        currencyfrom.innerHTML = selectedCurrency;
     } else {
         exchangeMoney = inMoney / rate;
-        currency = "ドル";
+        currency = selectedCurrency;
+        var currencyfrom = document.getElementById("currencyFrom");
+        currencyfrom.innerHTML = "円";
 
+    }
+
+    exchangeMoney = Math.round(exchangeMoney * 100) / 100;
+
+    if (isNaN(exchangeMoney)) {
+        alert("数値を入力してください！");
+        exchangeMoney = "- - -";
+    }
+
+    if (rate <= 0) {
+        alert("為替レートには０より大きな数値を入力してください！");
+        exchangeMoney = "- - -";
     }
 
     var outMoneyElement = document.getElementById("outMoney");
